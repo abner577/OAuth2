fcd7e3a5de986d249fc52de5c5ecc2b42a5d82b4
Abner Rodriguez <abner07282005@gmail.com>
2025-09-19 13:24:24 -0400

    Initial Commit

---
+++ src/test/java/dev/abner/OAuth2_login/OAuth2LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abner.OAuth2_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
