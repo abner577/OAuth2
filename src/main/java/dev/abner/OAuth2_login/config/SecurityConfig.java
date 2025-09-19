e1e8f03326dfdeb4d6812eab3887aae717717273
Abner Rodriguez <abner07282005@gmail.com>
2025-09-19 13:53:42 -0400

    Added Oauth2 login with GitHub

---
+++ src/main/java/dev/abner/OAuth2_login/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package dev.abner.OAuth2_login.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
